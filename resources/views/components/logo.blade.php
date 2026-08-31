0baf5381b1a60d17a9d151ae5b7d43fd54d1c207
Kemal Atayev <kemalatayew913@gmail.com>
2026-08-31 13:44:06 +0500

    1.58

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -3,8 +3,8 @@
 @if($businessSettings->logo_url)
     <img src="{{ $businessSettings->logo_url }}" alt="{{ $businessSettings->site_name }}" style="height:{{ $height }}px;width:auto;max-width:190px;object-fit:contain;display:block" {{ $attributes }}>
 @elseif($forceDark)
-    <img src="{{ asset('logo-full-dark.png') }}" alt="AK KAGYZ" style="height:{{ $height }}px;width:auto;display:block;" {{ $attributes }}>
+    <img src="{{ asset('logo-full-dark.png') }}" alt="{{ $businessSettings->site_name }}" style="height:{{ $height }}px;width:auto;display:block;" {{ $attributes }}>
 @else
-    <img src="{{ asset('logo-full.png') }}" alt="AK KAGYZ" class="ak-logo-light" style="height:{{ $height }}px;width:auto;display:block;" {{ $attributes }}>
-    <img src="{{ asset('logo-full-dark.png') }}" alt="AK KAGYZ" class="ak-logo-dark" style="height:{{ $height }}px;width:auto;display:none;" {{ $attributes }}>
+    <img src="{{ asset('logo-full.png') }}" alt="{{ $businessSettings->site_name }}" class="ak-logo-light" style="height:{{ $height }}px;width:auto;display:block;" {{ $attributes }}>
+    <img src="{{ asset('logo-full-dark.png') }}" alt="{{ $businessSettings->site_name }}" class="ak-logo-dark" style="height:{{ $height }}px;width:auto;display:none;" {{ $attributes }}>
 @endif
