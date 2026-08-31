b8a347a2e41bc5320e56ab54d329e9bb3d6868b8
Kemal Atayev <kemalatayew913@gmail.com>
2026-08-31 14:56:10 +0500

    1.53

--- resources/views/storefront/partials/cart-drawer-items.blade.php
+++ resources/views/storefront/partials/cart-drawer-items.blade.php
@@ -1,8 +1,24 @@
 @forelse($items as $item)
     @php($image = $item->product->images->first())
+    @php($itemUrl = route($item->product->seller_id ? 'marketplace.products.show' : 'products.show', $item->product->slug))
     <div class="cart-drawer-item">
-        <a class="cart-drawer-image" href="{{ route($item->product->seller_id ? 'marketplace.products.show' : 'products.show', $item->product->slug) }}" @if($image) style="background-image:url('{{ $image->url }}')" @endif>@unless($image)<x-icon name="image" :size="22"/>@endunless</a>
-        <div><a class="cart-drawer-name" href="{{ route($item->product->seller_id ? 'marketplace.products.show' : 'products.show', $item->product->slug) }}">{{ $item->product->name }}</a><div class="cart-drawer-qty">{{ $item->quantity }} шт. × {{ number_format($item->product->price,0,'',' ') }} TMT</div></div>
+        <a class="cart-drawer-image" href="{{ $itemUrl }}" @if($image) style="background-image:url('{{ $image->url }}')" @endif>@unless($image)<x-icon name="image" :size="22"/>@endunless</a>
+        <div class="cart-drawer-item-body">
+            <a class="cart-drawer-name" href="{{ $itemUrl }}">{{ $item->product->name }}</a>
+            <div class="cart-drawer-qty">{{ number_format($item->product->price,0,'',' ') }} TMT / шт.</div>
+            <div class="cart-drawer-item-bottom">
+                <form method="POST" action="{{ route('cart.add', $item->product) }}" class="cart-ajax-form cart-drawer-stepper" data-product-id="{{ $item->product->id }}">
+                    @csrf
+                    <button type="submit" data-cart-action="decrement" aria-label="Уменьшить количество"><x-icon name="minus" :size="12"/></button>
+                    <span>{{ $item->quantity }}</span>
+                    <button type="submit" data-cart-action="increment" aria-label="Увеличить количество" {{ $item->quantity >= $item->product->stock ? 'disabled' : '' }}><x-icon name="plus" :size="12"/></button>
+                </form>
+                <form method="POST" action="{{ route('cart.destroy', $item) }}" class="cart-ajax-form" data-product-id="{{ $item->product->id }}">
+                    @csrf @method('DELETE')
+                    <button type="submit" data-cart-action="remove" class="cart-drawer-remove" aria-label="Удалить товар"><x-icon name="trash" :size="13"/></button>
+                </form>
+            </div>
+        </div>
         <div class="cart-drawer-price">{{ number_format($item->quantity * $item->product->price,0,'',' ') }} TMT</div>
     </div>
 @empty
