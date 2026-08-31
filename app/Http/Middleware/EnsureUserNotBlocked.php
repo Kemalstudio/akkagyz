5c9cb0e8cbf2cfcb1ad954c07e18f4829975b591
Kemal Atayev <kemalatayew913@gmail.com>
2026-08-31 10:54:38 +0500

    1.20

---
+++ app/Http/Middleware/EnsureUserNotBlocked.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserNotBlocked
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->is_blocked) {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            return redirect()->route('login');
+        }
+
+        return $next($request);
+    }
+}
