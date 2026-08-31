4a3255ef945df450713637a9e8786ec4630fdacd
Kemal Atayev <kemalatayew913@gmail.com>
2026-08-31 11:03:39 +0500

    1.44

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -116,6 +116,7 @@ class ProductController extends Controller
     {
         $data = $this->validated($request);
         $data['is_vip'] = $request->boolean('is_vip');
+        $data['condition'] ??= 'new';
         $attributeValues = $data['attribute_values'] ?? [];
         unset($data['attribute_values']);
 
